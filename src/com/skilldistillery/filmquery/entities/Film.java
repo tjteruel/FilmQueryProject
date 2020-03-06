7669de12b76f24f5b3b818458cb57491f6cec5f2
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-03-06 16:44:04 -0700

    initialize

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	//if it can be null in the database, it will be a wrapper class
+	private int id;
+	private double replacementCost;
+	private String rating;
+	private Integer releaseYear;
+	private String specialFeatures;
+	
+	 //generate getters and setters and tostring
+}
