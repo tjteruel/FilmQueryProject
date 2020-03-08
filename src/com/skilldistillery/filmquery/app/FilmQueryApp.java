0ab76100f2cc1dead563c862d077cb46e3186164
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-03-08 17:41:00 -0600

    working on usermenu

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.filmquery.app;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
@@ -7,30 +9,69 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-  }
+	public static void main(String[] args) {
+		FilmQueryApp app = new FilmQueryApp();
+		//app.test();
+		app.launch();
+	}
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+//	private void test() throws SQLException {
+//		Film film = db.findFilmById(1);
+//		System.out.println(film);
+//	}
 
-  private void startUserInterface(Scanner input) {
-    
-  }
+	private void launch() {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) {
+		System.out.println("\t\t Welcome to the Film Database!");
+		boolean menuLoop = true;
+		while (menuLoop) {
+			System.out.println("");
+			System.out.println("\t What would you like to do? ");
+			System.out.println("1. Look Up Film By ID Number. ");
+			System.out.println("2. Look Up Film By Key Word. ");
+			System.out.println("3. Exit. ");
+			
+		//TODO:fix scanner/userChoice
+			switch (userChoice) {
+			case 1:
+				System.out.print("Please enter the Film ID: ");
+				int id = input.nextInt();
+				Film filmID = null;
+				try {
+					filmID = db.findFilmById(id);
+				} catch (SQLException e) {
+					System.err.println("Please enter choices 1-3. ");
+				}
+				if (filmID == null) {
+					System.err.println("Film not found.");
+				} else {
+					System.out.println(filmID);
+				}
+				break;
+			case 2:
+				System.out.print("Please enter the keyword: ");
+				String keyword = input.nextLine();
+				List<Film> film = db.findFilmByKeyword(keyword);
+				System.out.println(film);
+				break;
+			case 3:
+				menuLoop = false;
+				System.out.println("/t Thank You For Using The Database!");
+				break;
+			default:
+				System.out.println("Please enter an option of 1-3. ");
+			}
+		}
+	}
 
 }
--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,12 +1,114 @@
 package com.skilldistillery.filmquery.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class DatabaseAccessorObject implements DatabaseAccessor {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
 
-  @Override
-  public Film findFilmById(int filmId) {
-    return null;
-  }
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//returns film by ID
+	@Override
+	public Film findFilmById(int filmId) {
+		Film film = null;
+		String user = "student";
+		String pass = "student";
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			String sql = "SELECT * FROM film WHERE id = ?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, filmId);
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				film = new Film();
+				film.setTitle(rs.getString("title"));
+				film.setReleaseYear(rs.getInt("release_year"));
+				film.setRating(rs.getString("rating"));
+				film.setDescription(rs.getString("description"));
+			}
+			rs.close();
+			pst.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return film;
+
+	}
+	
+	//returns films by keyword, displays title, year, rating, description and language
+	public List<Film> findFilmByKeyword(String keyword) {
+		String searchWord = "%" + keyword + "%";
+		List<Film> films = new ArrayList<>();
+		String user = "student";
+		String pass = "student";
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+			String sql = "SELECT * FROM film JOIN language ON film.language_id = language.id WHERE title like = ? or description like = ?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, searchWord);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				((Film) films).setTitle(rs.getString("title"));
+				((Film) films).setReleaseYear(rs.getInt("release_year"));
+				((Film) films).setRating(rs.getString("rating"));
+				((Film) films).setDescription(rs.getString("description"));
+			}
+			rs.close();
+			pst.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return films;
+
+	}
+
+	//returns Actor by ID
+	@Override
+	public Actor findActorById(int actorId) {
+		Actor actor = null;
+		String user = "student";
+		String pass = "student";
+			try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+				String sql = "SELECT * FROM actor WHERE id = ?";
+
+		  PreparedStatement pst = conn.prepareStatement(sql);
+		  pst.setInt(1,actorId);
+		  ResultSet rs = pst.executeQuery();
+		  if (rs.next()) {
+		    actor = new Actor();
+		    actor.setId(rs.getInt("id"));
+		    actor.setFirstName(rs.getString("first_name"));
+		    actor.setLastName(rs.getString("last_name"));
+		  }
+		  	rs.close();
+			pst.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		  return actor;	
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
